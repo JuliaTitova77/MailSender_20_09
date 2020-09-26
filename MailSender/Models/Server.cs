8b0a990e5207540471bd44a0db96b7a50064ea84
julia77 <julia.titova@outlook.com>
2020-09-26 23:01:12 +0300

    основные модели данных

---
+++ MailSender/Models/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.Models
+{
+    class Server
+    {
+        public string Address { get; set; }
+        private int Port;
+        public int _Port
+        {
+            get => _Port;
+            set
+            {
+                if (value < 0 || value >=65535)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Номер порта должен быть в диапазоне от 0 до 65535");
+                }
+                _Port = value;
+            }
+        }
+        
+        public bool UseSSL { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+
+    }
+}
