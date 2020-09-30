548f288109130bf2e118932920eca33818cc3129
julia77 <julia.titova@outlook.com>
2020-09-30 18:25:27 +0300

    Реализация модели представления

---
+++ Tests/WpfTests/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace WpfTests.ViewModels.Base
+{
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
---
+++ Tests/WpfTests/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfTests.ViewModels.Base;
+
+
+namespace WpfTests.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Тестовое окно";
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if (_Title == value) return;
+                _Title = value;
+                OnPropertyChanged("Title");
+                {
+
+                }
+            }
+        }
+    }
+}
