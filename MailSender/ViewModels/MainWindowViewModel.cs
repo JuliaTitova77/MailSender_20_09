21cec7ad86f913fbf23f3237d215c110cb00fef1
julia77 <julia.titova@outlook.com>
2020-09-30 21:09:55 +0300

    Перенос инфраструктуры в MailSender

---
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MailSender.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Тестовое окно";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+            
+        }
+    }
+}
