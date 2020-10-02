b3d2a91c7a52dec5e931a05f4985b8e37e7f56fa
julia77 <julia.titova@outlook.com>
2020-10-02 12:42:02 +0300

    Команды управления серверами

--- MailSender/ViewModels/MainWindowViewModel.cs
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,10 @@
 using MailSender.Models;
 using System.Collections.ObjectModel;
 using MailSender.Data;
+using System.Windows.Input;
+using MailSender.Infrastructure.Commands;
+using System.Windows;
+using System.Linq;
 
 namespace MailSender.ViewModels
 {
@@ -72,7 +76,70 @@ namespace MailSender.ViewModels
             set => Set(ref _SelectedMessage, value);
         }
 
-       
+        #region Команды
+        
+        #region CreateNewServerCommand
+
+        //Создание нового сервера
+
+        private ICommand _CreateNewServerCommand;
+
+        public ICommand CreateNewServerCommand => _CreateNewServerCommand
+            ??= new LambdaCommand(OnCreateNewServerCommandExecuted, CanCreateNewServerCommandExecute);
+
+        private bool CanCreateNewServerCommandExecute(object p)=> true;
+
+        private void OnCreateNewServerCommandExecuted(object p)
+        {
+            MessageBox.Show("Создание нового сервера", "Управление серверами");
+        }
+
+        #endregion
+
+        #region EditNewServerCommand
+
+        //редактирование нового сервера
+
+        private ICommand _EditNewServerCommand;
+
+        public ICommand EditNewServerCommand => _EditNewServerCommand
+            ??= new LambdaCommand(OnEditNewServerCommandExecuted, CanEditNewServerCommandExecute);
+
+        private bool CanEditNewServerCommandExecute(object p) => p is Server || SelectedServer!= null;
+
+        private void OnEditNewServerCommandExecuted(object p)
+        {
+            var server = p as Server ?? SelectedServer;
+            if (server is null) return; 
+            MessageBox.Show($"Редактирование сервера {server.Address}", "Управление серверами");
+        }
+
+        #endregion
+
+        #region DeleteNewServerCommand
+
+        //удаление нового сервера
+
+        private ICommand _DeleteNewServerCommand;
+
+        public ICommand DeleteNewServerCommand => _DeleteNewServerCommand
+            ??= new LambdaCommand(OnDeleteNewServerCommandExecuted, CanDeleteNewServerCommandExecute);
+
+        private bool CanDeleteNewServerCommandExecute(object p) => p is Server || SelectedServer != null;
+
+        private void OnDeleteNewServerCommandExecuted(object p)
+        {
+            var server = p as Server ?? SelectedServer;
+            if (server is null) return;
+            Servers.Remove(server); // удаляем сервер из списка
+            SelectedServer = Servers.FirstOrDefault();// сбрасываем SelectedServer
+      
+            MessageBox.Show($"Удаление сервера {server.Address}", "Управление серверами");
+        }
+
+        #endregion
+
+        #endregion
         public MainWindowViewModel()
         {
             Servers = new ObservableCollection<Server>(TestData.Servers);
