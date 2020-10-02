19b0a317bbbd291266f3098d52ba7e0840257f87
julia77 <julia.titova@outlook.com>
2020-10-02 10:35:55 +0300

    Модель-представления главного окна основного проекта

--- MailSender/ViewModels/MainWindowViewModel.cs
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using MailSender.ViewModels.Base;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using MailSender.Models;
+using System.Collections.ObjectModel;
+using MailSender.Data;
 
 namespace MailSender.ViewModels
 {
@@ -13,6 +13,72 @@ namespace MailSender.ViewModels
             get => _Title;
             set => Set(ref _Title, value);
             
+        }
+
+        //в данных полях модель представления будет хранить свои данные
+        private ObservableCollection<Server> _Servers;
+        private ObservableCollection<Sender> _Senders;
+        private ObservableCollection<Recipient> _Recipients;
+        private ObservableCollection<Message> _Messages;
+
+        public ObservableCollection<Sender> Senders { 
+            get=> _Senders;
+            set=> Set(ref _Senders,value);
+        }
+
+        public ObservableCollection<Recipient> Recipients
+        {
+            get => _Recipients;
+            set => Set(ref _Recipients, value);
+        }
+
+        public ObservableCollection<Message> Messages
+        {
+            get => _Messages;
+            set => Set(ref _Messages, value);
+        }
+
+        public ObservableCollection<Server> Servers
+        {
+            get => _Servers;
+            set => Set(ref _Servers, value);
+        }
+
+        private Server _SelectedServer;
+        public Server SelectedServer
+        {
+            get => _SelectedServer;
+            set => Set(ref _SelectedServer, value);
+        }
+
+        private Sender _SelectedSender;
+        public Sender SelectedSender
+        {
+            get => _SelectedSender;
+            set => Set(ref _SelectedSender, value);
+        }
+
+        private Recipient _SelectedRecipient;
+        public Recipient SelectedRecipient
+        {
+            get => _SelectedRecipient;
+            set => Set(ref _SelectedRecipient, value);
+        }
+
+        private Message _SelectedMessage;
+        public Message SelectedMessage
+        {
+            get => _SelectedMessage;
+            set => Set(ref _SelectedMessage, value);
+        }
+
+       
+        public MainWindowViewModel()
+        {
+            Servers = new ObservableCollection<Server>(TestData.Servers);
+            Senders = new ObservableCollection<Sender>(TestData.Senders);
+            Recipients = new ObservableCollection<Recipient>(TestData.Recipients);
+            Messages = new ObservableCollection<Message>(TestData.Messages);
         }
     }
 }
