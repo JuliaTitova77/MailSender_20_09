68bcdbd95b3ebe5fcc6dc3861c32ba6987ac1c9f
julia77 <julia.titova@outlook.com>
2020-10-08 11:19:13 +0300

    Представление списка получателей почты

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MailSender.lib.Interfaces;
+using MailSender.lib.Service;
+using System;
 using System.Net;
 using System.Net.Mail;
 
@@ -9,23 +11,11 @@ namespace ConsoleTests
         static void Main(string[] args)
         {
 
-            var to = new MailAddress("julia.titova@gmail.com","Юлия");
-            var from = new MailAddress("julia.titova@outlook.com", "Юлия");
-
-            var message = new MailMessage(from, to);
-
-            message.Subject = "Заголовок письма от " + DateTime.Now;
-            message.Body = "Текст тестового письма + " + DateTime.Now;
-            var client = new SmtpClient("smtp.yandex.ru", 587);
-            
-            client.Credentials = new NetworkCredential
-            {
-                UserName = "user_name",
-                Password = "Password!"
-
-            };
-            client.Send(message);
-            Console.WriteLine("Hello World!");
+            IEncryptorService cryptor = new Rfc_2898Encryptor();
+            var str = "Hello world!";
+            const string password = "MailSender!";
+            var crypted_str = cryptor.Encrypt(str, password);
+            var decryptor_str = cryptor.Decrypt(crypted_str, password);
         }
     }
 }
