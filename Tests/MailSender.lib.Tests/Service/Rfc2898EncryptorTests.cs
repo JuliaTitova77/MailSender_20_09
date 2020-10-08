bc610e89d92afcfd45097ac74e0b5f847cce0807
julia77 <julia.titova@outlook.com>
2020-10-08 21:10:09 +0300

    Перепроверка тестов

--- Tests/MailSender.lib.Tests/Service/Rfc2898EncryptorTests.cs
+++ Tests/MailSender.lib.Tests/Service/Rfc2898EncryptorTests.cs
@@ -1,54 +1,53 @@
-﻿using MailSender.lib.Interfaces;
+﻿using System.Security.Cryptography;
+using MailSender.lib.Interfaces;
 using MailSender.lib.Service;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace MailSender.lib.Tests.Service
-{   
-    
+{
     [TestClass]
     public class Rfc2898EncryptorTests
     {
         private IEncryptorService _Encryptor = new Rfc2898Encryptor();
 
         static Rfc2898EncryptorTests() { }
-        public Rfc2898EncryptorTests()
-        {
 
-        }
+        public Rfc2898EncryptorTests() { }
+
         [AssemblyInitialize]
-        public static void AssemblyInitialize(TestContext context)
+        public static void AssemblyInitialize(TestContext Context)
         {
 
         }
 
         [ClassInitialize]
-        public static void ClassInitialize(TestContext context)
+        public static void ClassInitialize(TestContext Context)
         {
 
         }
+
         [TestInitialize]
         public void TestInitialize()
         {
 
         }
-        //Выполняется модульный тест
+
+        // Выполняется сам модульный тест
+
         [TestCleanup]
-        public void TestCleanUp()
+        public void TestCleanup()
         {
 
         }
 
         [ClassCleanup]
-        public static void ClassCleanUp(TestContext context)
+        public static void ClassCleanup()
         {
 
         }
+
         [AssemblyCleanup]
-        public static void AssemblyCleanUp(TestContext context)
+        public static void AssemblyCleanup()
         {
 
         }
@@ -56,31 +55,27 @@ namespace MailSender.lib.Tests.Service
         [TestMethod]
         public void Encrypt_Hello_World_and_Decrypt_with_Password()
         {
-            const string str = "Hello world";
+            const string str = "Hello World!";
             const string password = "Password";
 
             var encrypted_str = _Encryptor.Encrypt(str, password);
+
             var decrypted_str = _Encryptor.Decrypt(encrypted_str, password);
-            //var wrong_pass_decrypted = _Encryptor.Decrypt(encrypted_str, "QWE");
+            //var vrong_pass_decrypted = _Encryptor.Decrypt(encrypted_str, "QWE");
 
             Assert.AreNotEqual(str, encrypted_str);
             Assert.AreEqual(str, decrypted_str);
-            //Assert.AreNotEqual(str, wrong_pass_decrypted);
+            //Assert.AreNotEqual(str, vrong_pass_decrypted);
         }
 
-        [TestMethod,ExpectedException(typeof(CryptographicException))]
-        public void Wrong_Decryption_thrown_CryptografyExeption() 
+        [TestMethod, ExpectedException(typeof(CryptographicException))]
+        public void Wrong_Decryption_thrown_CryptographicException()
         {
-            const string str = "Hello world";
+            const string str = "Hello World!";
             const string password = "Password";
 
             var encrypted_str = _Encryptor.Encrypt(str, password);
-            
-            var wrong_pass_decrypted = _Encryptor.Decrypt(encrypted_str, "QWE");
-           
+            var vrong_pass_decrypted = _Encryptor.Decrypt(encrypted_str, "QWE");
         }
     }
-    
-
-
 }
