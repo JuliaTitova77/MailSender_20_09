c2fd09b4c56d38f875d7c4f3e65666b89380c5a1
julia77 <julia.titova@outlook.com>
2020-10-08 14:00:49 +0300

    Визуализация ошибок валидации

--- MailSender.lib/Models/Recipient.cs
+++ MailSender.lib/Models/Recipient.cs
@@ -1,9 +1,30 @@
 ﻿
 
 using MailSender.lib.Models.Base;
+using System;
 
 namespace MailSender.Models
 {
-    public class Recipient : Person { }
+    public class Recipient : Person
+    {
+        public override string Name 
+        { 
+            get => base.Name;
+            set
+            {
+                if (value is null)
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+                if (value == "")
+                {
+                    throw new ArgumentException("Имя не может быть пустой строкой", nameof(value));
+                }
+                if (value == "QWE")
+                    throw new ArgumentException("Запрещено вводить QWE!", nameof(value));
+                base.Name = value;
+            }
+        }
+    }
     
 }
--- MailSender/Views/RecipientEditor.xaml.cs
+++ MailSender/Views/RecipientEditor.xaml.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using MailSender.lib.Models;
+
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace MailSender.Views
 {
@@ -18,9 +10,20 @@ namespace MailSender.Views
     /// </summary>
     public partial class RecipientEditor : UserControl
     {
-        public RecipientEditor()
+        public RecipientEditor() => InitializeComponent();
+        
+
+        private void OnDataValidationError(object sender, ValidationErrorEventArgs e)
         {
-            InitializeComponent();
+            //Control control = (Control)Sender;
+            //if (e.Action == ValidationErrorEventAction.Added)
+            //{
+            //    control.ToolTip = e.Error.ErrorContent.ToString();
+            //}
+            //else
+            //{
+            //    control.ClearValue(ToolTipProperty);
+            //}
         }
     }
 }
