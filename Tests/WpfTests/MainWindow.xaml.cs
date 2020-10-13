270224e5aed71dbaa12850536347988d6053fdce
julia77 <julia.titova@outlook.com>
2020-10-13 13:15:55 +0300

    Обращение к элементам пользовательского интерфейса из потока id = 1

--- Tests/WpfTests/MainWindow.xaml.cs
+++ Tests/WpfTests/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace WpfTests
+﻿using System.Threading;
+using System.Windows;
+
+namespace WpfTests
 {   
     
     public partial class MainWindow
@@ -7,7 +10,30 @@
         {
             InitializeComponent();
         }
-        
-        
+
+        private void ComputerResultButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //new Thread(() => ResultText.Text = GetResultHard()).Start();// так нельзя использовать из потока в котором они не были созданы
+            new Thread(() =>
+            {
+                var result = GetResultHard();
+                Application.Current.Dispatcher.Invoke(() => ResultText.Text = result);//в этом случае приложение будет работать корректно и
+                //интерфейс виснуть не будет
+
+            })
+            { IsBackground = true}.Start();
+               
+               
+            
+        }
+
+        private string GetResultHard()
+        {
+            for(var i = 0; i < 500; i++)
+            {
+                Thread.Sleep(10);
+            }
+            return "Hello World";
+        }
     }
 }
