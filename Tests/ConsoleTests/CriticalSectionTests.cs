4b535ddb084c4a3004518e42ac2d510d050eaa4d
julia77 <julia.titova@outlook.com>
2020-10-13 12:28:34 +0300

    Синхронизация потоков с помощью lock. AutoResetEvent, ManualRestEvent

--- Tests/ConsoleTests/CriticalSectionTests.cs
+++ Tests/ConsoleTests/CriticalSectionTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 
 namespace ConsoleTests
 {
@@ -8,7 +11,93 @@ namespace ConsoleTests
     {
         public static void Start()
         {
+            //LockSyncronizationTest();
+            //эти объекты как стартовый пистолет для беговых дорожек, когда для страта в один период времени, либо сначала запустить потом приостановить
+            ManualResetEvent manual_reset_event = new ManualResetEvent(false);
+            AutoResetEvent autoResetEvent = new AutoResetEvent(false);
 
-        } 
+            //создаем перемменную базового класса и в нее записываем autoRestEvent
+            //EventWaitHandle starter = autoResetEvent;
+            EventWaitHandle starter = manual_reset_event;
+            for (var i = 0; i < 10; i++)
+            {
+                var local_i = i;
+                new Thread(() =>
+                {
+                    Console.WriteLine($"Поток {local_i} запущен");
+                    starter.WaitOne();
+                    Console.WriteLine($"Поток {local_i} завершил свою работу");
+                    starter.Reset();// исп при manual_reset_event
+                }).Start();
+            }
+            Console.WriteLine("Все потоки созданы и готовы к работе");
+            Console.ReadLine();
+                
+            starter.Set();
+            Console.ReadLine();
+
+           
+
+        }
+        private static void LockSyncronizationTest()
+        {
+            var threads = new Thread[10];
+            for (var i = 0; i < threads.Length; i++)
+            {
+                var local_i = i;
+                threads[i] = new Thread(() => PrintData($"Message from thread {local_i}", 10));
+                //так нельзя писать, происходить замыкание из-за лямбда выражения, которое формирует свое сама внутри
+            }
+            for (var i = 0; i < threads.Length; i++)
+            {
+                threads[i].Start();
+            }
+        }
+        private static readonly object __SyncRoot = new object();
+
+        private static void PrintData(string Message, int Count)
+        {
+            for (var i = 0; i < Count; i++)
+            {
+                lock (__SyncRoot)
+                {
+                    Console.Write("Thread id: {0};", Thread.CurrentThread.ManagedThreadId);
+                    Console.Write("\t");
+                    Console.Write(Message);
+                    Console.WriteLine();
+
+                }
+                //Синоним по действиям lock, Монитор исп. тогда когда нельзя применять lock в асинхронных потоках 
+                //Monitor.Enter(__SyncRoot);
+                //try
+                //{
+                //    Console.Write("Thread id: {0};", Thread.CurrentThread.ManagedThreadId);
+                //    Console.Write("\t");
+                //    Console.Write(Message);
+                //    Console.WriteLine();
+                //}
+                //finally
+                //{
+                //    Monitor.Exit(__SyncRoot);
+                //}
+            }
+        }
+    }
+    //класс который пишет данные в журнал
+    //сделать его синхронизированным, через механизм отправки ему сообщений через один поток
+   // [Syncronization] остался для .NET Framework!!!
+    public class FileLogger:ContextBoundObject
+    {
+        private readonly string _LogFileName;
+        public FileLogger(string LogFileName)
+        {
+            _LogFileName = LogFileName;
+
+        }
+        public void Log(string Message)
+        {
+            File.WriteAllText(_LogFileName, Message);
+        }
+
     }
 }
