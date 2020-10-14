3a318a60a910eb0ff6c2a9c6f1c1c06ec13af82c
julia77 <julia.titova@outlook.com>
2020-10-15 00:21:56 +0300

    Написано приложение, считающее в раздельных потоках: a. факториал числа N, которое вводится с клавиатуру; b. сумму целых чисел до N, которое также вводится с клавиатуры.

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -11,7 +11,15 @@ namespace ConsoleTests
         {
             //CriticalSectionTests.Start();
             //ThreadTests.Start();
-            ThreadPoolTests.Start();
+            //ThreadPoolTests.Start();
+           
+            Console.Write($"Введите число для расчетов N: ");
+            var N = Int32.Parse(Console.ReadLine());
+            //ThreadCalculation.Run(N);
+            Thread thread1 = new Thread(ThreadCalculation.MulResult);
+            Thread thread2 = new Thread(ThreadCalculation.SumResult);
+            thread1.Start(N);
+            thread2.Start(N);
             Console.WriteLine("Главный поток работу закончил");
             Console.ReadLine();
         }
---
+++ Tests/ConsoleTests/ThreadCalculation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleTests
+{
+    class ThreadCalculation
+    {
+        public static int ResultMul = 1;
+        public static int ResultSum = 0;      
+
+       
+
+        public static void MulResult(object count)
+        {
+            var data = (int)count;
+
+            while (data > 0)
+            {
+                ResultMul *= data;
+                data--;
+            }
+            Thread.Sleep(100);
+            Console.WriteLine($"Вычисление факториала числа {(int)count}");
+            Console.WriteLine($"Факториал числа {(int)count} = {ResultMul}");
+            Console.WriteLine($"Поток #1 работу окончил");
+        }
+            
+           
+        public static void SumResult(object count)
+        {
+            var data = (int)count;
+
+            while (data > 0)
+            {
+                ResultSum += data;
+                data--;
+            }
+
+            Thread.Sleep(100);
+            Console.WriteLine($"Вычисление суммы целых чисел {(int)count}");
+            Console.WriteLine($"Сумма целых чисел {(int)count} = {ResultSum}");
+            Console.WriteLine($"Поток #2 работу окончил");
+
+        }
+    }
+}
