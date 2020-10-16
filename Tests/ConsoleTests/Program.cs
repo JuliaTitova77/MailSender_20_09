ea0b8de990fb29b2a4bbfe71511afd9c57975e2d
julia77 <julia.titova@outlook.com>
2020-10-16 18:55:33 +0300

    Пример использования async-aw

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -12,8 +12,14 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
-            TPLOverview.Start();
+            //TPLOverview.Start();
+            var task = AsyncAwaitTest.StartAsync();//таск формируется самостоятельно компилятором (а так сами бы ручкаим делали 
+            //с помощью ключевого слова new
+            var process_manages = AsyncAwaitTest.ProcedssDataTestAsync();
+            Console.WriteLine("Тестовая задача запущена");
 
+            // task.Wait();
+            Task.WaitAll(task, process_manages);
             Console.WriteLine("Главный поток работу закончил");
             Console.ReadLine();
         }
