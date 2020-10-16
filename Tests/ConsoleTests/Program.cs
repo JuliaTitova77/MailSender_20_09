4cb44c83fd3071181e4307b40d5c25dcfac86d2d
julia77 <julia.titova@outlook.com>
2020-10-16 14:03:19 +0300

    Создан класс MulMatrixTPL, в котором сделаны: -метод FillMatrix- рандомно заполняет двумерную матрицу -MulMatrix перемножение двух матриц -Print печать матрицы

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -12,7 +12,14 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
-            TPLOverview.Start();
+            int[,] first = new int[10, 10];
+            int[,] second = new int[10, 10];
+            int[,] result = new int[10, 10];
+            //TPLOverview.Start();
+            var task = Task.Run(() => MulMatrixTPL.FillMatrix(first));
+            var cont_task = task.ContinueWith(t => MulMatrixTPL.FillMatrix(second));       
+            cont_task.ContinueWith(t => MulMatrixTPL.MulMatrix(first, second, result));
+            cont_task.ContinueWith(t => MulMatrixTPL.Print(result, "Перемножение двух матриц"));
 
             Console.WriteLine("Главный поток работу закончил");
             Console.ReadLine();
