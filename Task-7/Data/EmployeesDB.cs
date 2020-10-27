976dd5bd86f73bf533684c151cd6aff782777a77
julia77 <julia.titova@outlook.com>
2020-10-27 15:05:02 +0300

    Создан класс Employee и клас EmployeesDB для подключения к БД

---
+++ Task-7/Data/EmployeesDB.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Task_7.Data.Entities;
+
+namespace Task_7.Data
+{
+    public class EmployeesDB : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public EmployeesDB(DbContextOptions<EmployeesDB> options) : base(options) { }// подключение к БД
+    }
+}
---
+++ Task-7/Data/Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Task_7.Data.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+    }
+    public abstract class DataEntity : Entity
+    {
+        [Required]
+        public string Name { get; set; }
+
+    }
+    public class Employee : DataEntity
+    {
+        public string Email { get; set; }
+        [Required, MaxLength(20)]
+        public string PhoneNumber { get; set; }
+        public virtual Group Group { get; set; }// навигационное свойство
+    }
+
+    public class Group : DataEntity
+    {
+        public virtual ICollection<Employee> Employees { get; set; }// навигационное свойство
+    }
+}
